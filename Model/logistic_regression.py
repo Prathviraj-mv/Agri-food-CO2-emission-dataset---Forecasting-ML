e61c7dd6a4085f1a2397d4d67a30c2256dc11296
Prathviraj-mv <chappie.peachu@gmail.com>
2026-07-12 23:06:21 +0530

    lr

--- Model/logistic_regression.py
+++ Model/logistic_regression.py
@@ -1,1 +1,11 @@
+from sklearn.linear_model import LogisticRegression
+# logistic regression
+
+from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.model_selection import GridSearchCV
+import joblib
+from Helpers.IOdata import IO
+from Helpers.plotters import PLOT
+from Definitions.constants import OUTPUT_DIR_lr
+
 
