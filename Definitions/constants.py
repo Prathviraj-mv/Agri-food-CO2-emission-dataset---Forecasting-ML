40d69a8674a32e6894162b8d94730ec3dfcb3fa7
Prathviraj-mv <chappie.peachu@gmail.com>
2026-07-13 13:14:33 +0530

    kn

--- Definitions/constants.py
+++ Definitions/constants.py
@@ -12,4 +12,6 @@ OUTPUT_DIR_p = ROOT_DIR / "EDA Plots"
 OUTPUT_DIR_rf = ROOT_DIR / "Model Plots" /"RD"
 OUTPUT_DIR_lr = ROOT_DIR / "Model Plots" / "LF"
 OUTPUT_DIR_xg = ROOT_DIR / "Model Plots" / "XG"
+OUTPUT_DIR_kn = ROOT_DIR / "Model Plots" / "KN"
+
 
